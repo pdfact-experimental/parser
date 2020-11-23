e37f39eff8c811da8c5ce0b159f802424963eda1
ckorzen <ckorzen@users.noreply.github.com>
2020-11-23 16:44:40 +0000

    Introduced aggregrate pom.xml

--- src/main/java/pdfact/PdfParserMain.java
+++ src/main/java/pdfact/PdfParserMain.java
@@ -1,12 +1,7 @@
 package pdfact;
 
-import pdfact.model.Document;
-import pdfact.parser.PdfParser;
-
 public class PdfParserMain {
   public static void main(String[] args) {
-    System.out.println("Running the PDF parser ...");
-    Document doc = new PdfParser().parse();
-    System.out.println("Parsed document: " + doc.id);
+    System.out.println("Hello");
   }
 }
