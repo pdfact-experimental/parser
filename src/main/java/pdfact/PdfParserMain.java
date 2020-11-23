35925fb0eaaaaa745820ec054e3abb7021a1037c
ckorzen <ckorzen@users.noreply.github.com>
2020-11-23 13:52:52 +0000

    Added debug output.

--- src/main/java/pdfact/PdfParserMain.java
+++ src/main/java/pdfact/PdfParserMain.java
@@ -5,7 +5,8 @@ import pdfact.parser.PdfParser;
 
 public class PdfParserMain {
   public static void main(String[] args) {
+    System.out.println("Running the PDF parser ...");
     Document doc = new PdfParser().parse();
-    System.out.println("Document: " + doc.id);
+    System.out.println("Parsed document: " + doc.id);
   }
 }
